2663191c71a7a7e5d2dcadf92b3a5bf0df5ed001
Satish Rajanna <satish.rpr@gmail.com>
2025-01-05 20:20:36 +0530

    changes to updatereadme.py

--- .github/workflows/UpdateReadme.py
+++ .github/workflows/UpdateReadme.py
@@ -1,6 +1,6 @@
 import os
 import subprocess
 
-result = subprocess.run(['git tag'], shell=True, capture_output=True, text=True).stdout
+result = subprocess.run(['git tag'], shell=True, capture_output=True, text=True, cwd='../../').stdout
 
 print(result)
