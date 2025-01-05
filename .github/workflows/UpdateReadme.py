14e08ce97373cafecbacca61b4fdeb64e7dc3c52
Satish Rajanna <satish.rpr@gmail.com>
2025-01-05 20:57:28 +0530

    changes to updatereadme.py

--- .github/workflows/UpdateReadme.py
+++ .github/workflows/UpdateReadme.py
@@ -2,7 +2,8 @@ import os
 import sys
 import subprocess
 
-
-result = subprocess.run(['git tag'], shell=True, capture_output=True, text=True, cwd=sys.argv[1]).stdout
+github_env = os.environ['GITHUB_ENV']
+result = subprocess.run(['la -l'], shell=True, capture_output=True, text=True, cwd=sys.argv[1]).stdout
 
 print(result)
+print (f'github env {github_env}')
