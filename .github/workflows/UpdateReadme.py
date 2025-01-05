4f3b269ab7140d1fa624145c3ae1e10b734c6fcd
Satish Rajanna <satish.rpr@gmail.com>
2025-01-05 20:02:02 +0530

    changes to updatereadme.py

--- .github/workflows/UpdateReadme.py
+++ .github/workflows/UpdateReadme.py
@@ -1,10 +1,6 @@
 import os
 import subprocess
 
-for root, dirs, files in os.walk("."):
-    for file in files:
-        print(f'file {os.path.join(root, file)}')
-
 result = subprocess.run(['git tag'], shell=True, capture_output=True, text=True).stdout
 
 print(result)
