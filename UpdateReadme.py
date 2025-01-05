f19ef215b0f733329f165ff1d3b86e843162e09c
Satish Rajanna <satish.rpr@gmail.com>
2025-01-05 19:33:35 +0530

    commit changes to UpdateReadme.py

--- UpdateReadme.py
+++ UpdateReadme.py
@@ -1,5 +1,10 @@
 import os
+import subporcess
 
 for root, dirs, files in os.walk("."):
     for file in files:
         print(f'file {os.path.join(root, file)}')
+
+result = subprocess.run(['git', 'tag']).decode('utf-8')
+
+print(result.splitlines())
