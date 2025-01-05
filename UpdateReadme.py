f13d643a80c8e413e3c7d422f5fd00737c13147c
Satish Rajanna <satish.rpr@gmail.com>
2025-01-05 19:37:01 +0530

    changes to UpdateReadme.py

--- UpdateReadme.py
+++ UpdateReadme.py
@@ -5,6 +5,6 @@ for root, dirs, files in os.walk("."):
     for file in files:
         print(f'file {os.path.join(root, file)}')
 
-result = subprocess.run(['git', 'tag']).decode('utf-8')
+result = subprocess.run(['git', 'tag'], stdout=subprocess.STDOUT)
 
 print(result.splitlines())
