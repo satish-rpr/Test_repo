a6a9c94dfdb242e7cdc93da536e973fe00c7856b
Satish Rajanna <satish.rpr@gmail.com>
2025-01-05 20:36:28 +0530

    changes to updatereadme.py

--- .github/workflows/UpdateReadme.py
+++ .github/workflows/UpdateReadme.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import subprocess
 
-result = subprocess.run(['ls -l'], shell=True, capture_output=True, text=True, cwd='../../').stdout
+
+result = subprocess.run(['ls -l'], shell=True, capture_output=True, text=True, cwd=sys.argv[1]).stdout
 
 print(result)
